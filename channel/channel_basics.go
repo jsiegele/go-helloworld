2d803ca55dbc9172e8b8182fd2bfb70a3b78618f
jsiegele <johann.siegele@gmail.com>
2020-02-20 19:30:41 +0100

    channel - For-Loop

--- channel/channel_basics.go
+++ channel/channel_basics.go
@@ -3,6 +3,7 @@ package main
 import(
 	"fmt"
 	"sync"
+	"time"
 )
 
 var wg = sync.WaitGroup{}
@@ -10,16 +11,21 @@ var wg = sync.WaitGroup{}
 func main()  {
 	ch := make(chan int)
 	for j := 0; j < 5; j++ {
+		fmt.Println("init:\t", j)
 		wg.Add(2)
 		go func(ch <-chan int) {
 			i := <- ch
-			fmt.Println(i)
+			fmt.Println("pop:\t", i)
 			wg.Done()
 		}(ch)
 		go func(ch chan<- int) {
-			ch <- 42
+			fmt.Println("push:\t", j)
+			ch <- j
 			wg.Done()
 		}(ch)
-	}
+		// interessant, wenn folgendes Sleep entfernt wird:
+		time.Sleep(100 * time.Millisecond)
+		// anstelle vom Sleep kann auch wg.Wait() in die Loop gezoen werden
+	}	  
 	wg.Wait()
 }
