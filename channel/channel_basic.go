e511b1243ccea33071d4fe57a0d7ef9b65d4e560
Johann Siegele <johann.siegele@arz.at>
2020-02-21 08:05:43 +0100

    Channel Basic hinzugefügt - Source: https://medium.com/rungo/anatomy-of-channels-in-go-concurrency-in-go-1ec336086adb

---
+++ channel/channel_basic.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func squares(c chan int) {
+	for i := 0; i <= 9; i++ {
+		c <- i * i
+	}
+	close(c)
+}
+
+func main() {
+	fmt.Println("main() started")
+	c := make(chan int)
+
+	go squares(c) // start goroutine
+	// periodic block/unblock of main goroutine until channel closes
+	for {
+		val, ok := <-c
+		if ok == false {
+			fmt.Println(val, ok, "<-- loop broke!")
+			break // exit break loop
+		} else {
+			fmt.Println(val, ok)
+		}
+	}
+	fmt.Println("main() stopped")
+}
