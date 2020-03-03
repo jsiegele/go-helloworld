acebcce563c8d70da17fbfdbde1f3a8bdc200312
Johann Siegele <johann.siegele@arz.at>
2020-03-03 09:49:55 +0100

    data-structure

---
+++ data-structure/list/linked_list.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+
+// Post struct
+type Post struct {
+	body        string
+	publishDate int64 // Unix timestamp
+	next        *Post
+}
+
+// Feed struct
+type Feed struct {
+	length int // we'll use it later
+	start  *Post
+	end    *Post
+}
+
+// Append new Post to List
+func (f *Feed) Append(newPost *Post) {
+	if f.length == 0 {
+		f.start = newPost
+		f.end = newPost
+	} else {
+		lastPost := f.end
+		lastPost.next = newPost
+		f.end = newPost
+	}
+	f.length++
+}
+
+// Remove Element from List
+func (f *Feed) Remove(publishDate int64) {
+	if f.length == 0 {
+		panic(errors.New("Feed is empty"))
+	}
+
+	var previousPost *Post
+	currentPost := f.start
+
+	for currentPost.publishDate != publishDate {
+		if currentPost.next == nil {
+			panic(errors.New("no such post found"))
+		}
+
+		previousPost = currentPost
+		currentPost = currentPost.next
+	}
+	previousPost.next = currentPost.next
+
+	f.length--
+}
+
+// Insert Element
+func (f *Feed) Insert(newPost *Post) {
+	if f.length == 0 {
+		f.start = newPost
+	} else {
+		var previousPost *Post
+		currentPost := f.start
+
+		for currentPost.publishDate < newPost.publishDate {
+			previousPost = currentPost
+			currentPost = previousPost.next
+		}
+
+		previousPost.next = newPost
+		newPost.next = currentPost
+	}
+	f.length++
+}
+
+// Inspect Feed
+func (f *Feed) Inspect() {
+	if f.length == 0 {
+		fmt.Println("Feed is empty")
+	}
+	fmt.Println("========================")
+	fmt.Println("Feed Length: ", f.length)
+
+	currentIndex := 0
+	currentPost := f.start
+
+	for currentIndex < f.length {
+		fmt.Printf("Item: %v - %v\n", currentIndex, currentPost)
+		currentPost = currentPost.next
+		currentIndex++
+	}
+	fmt.Println("========================")
+}
+
+func main() {
+	rightNow := time.Now().Unix()
+	f := &Feed{}
+	p1 := &Post{
+		body:        "Lorem ipsum",
+		publishDate: rightNow,
+	}
+	p2 := &Post{
+		body:        "Dolor sit amet",
+		publishDate: rightNow + 10,
+	}
+	p3 := &Post{
+		body:        "consectetur adipiscing elit",
+		publishDate: rightNow + 20,
+	}
+	p4 := &Post{
+		body:        "sed do eiusmod tempor incididunt",
+		publishDate: rightNow + 30,
+	}
+	f.Append(p1)
+	f.Append(p2)
+	f.Append(p3)
+	f.Append(p4)
+
+	f.Inspect()
+
+	newPost := &Post{
+		body:        "This is a new post",
+		publishDate: rightNow + 15,
+	}
+	f.Insert(newPost)
+	f.Inspect()
+}
